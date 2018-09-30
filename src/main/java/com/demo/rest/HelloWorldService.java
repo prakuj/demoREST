98f4a6a2bc5250c9d446b406fb11cba3e65c1ccb
i348491 <pankaj.kumar11@sap.com>
2018-09-30 23:16:50 +0530

    Adding index.html & welcome file list

--- src/main/java/com/demo/rest/HelloWorldService.java
+++ src/main/java/com/demo/rest/HelloWorldService.java
@@ -1,21 +1,44 @@
 package com.demo.rest;
- 
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
- 
+
 @Path("/hello")
 public class HelloWorldService {
- 
+
 	@GET
 	@Path("/{param}")
 	public Response getMsg(@PathParam("param") String msg) {
- 
+
 		String output = "Jersey say : " + msg;
- 
+
 		return Response.status(200).entity(output).build();
- 
+
 	}
- 
+
+	// This method is called if HTML and XML is not requested
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String sayPlainTextHello() {
+		return "Hello Jersey Plain";
+	}
+
+	// This method is called if XML is requested
+	@GET
+	@Produces(MediaType.TEXT_XML)
+	public String sayXMLHello() {
+		return "<?xml version=\"1.0\"?>" + "<hello> Hello Jersey" + "</hello>";
+	}
+
+	// This method is called if HTML is requested
+	@GET
+	@Produces(MediaType.TEXT_HTML)
+	public String sayHtmlHello() {
+		return "<html> " + "<title>" + "Hello Jersey" + "</title>" + "<body><h1>" + "Hello Jersey HTML" + "</h1></body>"
+				+ "</html> ";
+	}
 }
